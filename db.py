f8c0d40f62e04e7cb998a2b1c87e25cf3d57b60b
Rohit <rohitbhangale918@gmail.com>
2026-05-03 12:45:42 +0530

    Create database connection file

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
 
-def get_db_connection():
-    connection=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="Margdarshak"
-    )
-    return connection
+load_dotenv()
+
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
